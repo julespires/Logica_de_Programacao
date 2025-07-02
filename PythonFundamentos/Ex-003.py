c65d42dd1c766a42cd4996092ff4b2f2a83d1645
Jules Pires <julespires@hotmail.com>
2025-07-02 20:21:16 -0300

    Ex-003.py
    Terçeiro exercício na linguagem Python

---
+++ PythonFundamentos/Ex-003.py
@@ -0,0 +1,14 @@
+"""
+Programa que receba dois valores pelo usuário e mostra a soma entre eles.
+Autor:Jules do Nascimento Pires
+Ex:003
+Versão:V1
+Data:01/07/2025
+"""
+
+n1 = int(input("Primeiro valor:"))
+n2 = int(input("Segundo valor:"))
+
+soma = n1 + n2
+
+print("A soma entre {} + {} = {} ".format(n1,n2,soma))
