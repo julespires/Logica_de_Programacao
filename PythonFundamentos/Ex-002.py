38c2960645e6fd7e59a24ffb72b88960e7ed4780
Jules Pires <julespires@hotmail.com>
2025-07-02 20:17:46 -0300

    Update Ex-002.py

---
+++ PythonFundamentos/Ex-002.py
@@ -0,0 +1,11 @@
+"""
+Programa que recebe o nome de um usuário e mostra a impressão na tela.
+Autor:Jules do Nascimento Pires
+Ex:002
+Versão:V1
+Data:01/07/2025
+"""
+
+nome = input("Qual é o seu nome?")
+
+print("Olá, {} é um prazer te conhecer!".format(nome))
