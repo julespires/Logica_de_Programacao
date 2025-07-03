dbdd3143493d7b002f0ba6876851b3f7a9786046
Jules Pires <julespires@hotmail.com>
2025-07-03 13:11:12 -0300

    Ex-009
    Crie o exercício de número 9 de logica de programação em Python.

---
+++ PythonFundamentos/Ex-009.py
@@ -0,0 +1,23 @@
+'''
+ Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura)/2.
+ Autor:Jules do Nascimento Pires
+ Ex:009
+ Versão:V1
+ Data:03/07/2025
+'''
+
+# Biblioteca
+import os
+
+# Limpa a tela
+os.system('cls')
+
+# Entrada de dados
+base = float(input("Base do triângulo:"))
+altura = float(input("Altura do triângulo:"))
+
+# Cálculo
+area = (base * altura) / 2
+
+# Resultado
+print("O triângulo tem uma área de {}cm".format(area))
