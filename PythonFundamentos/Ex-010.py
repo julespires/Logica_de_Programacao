ada9a63679d2f1583151351728c5c0046835dab3
Jules Pires <julespires@hotmail.com>
2025-07-03 13:32:44 -0300

    Ex-0010.py
    Criei o execercício de número 10

---
+++ PythonFundamentos/Ex-010.py
@@ -0,0 +1,23 @@
+'''
+Faça um programa que calcule e mostre a área de um círculo. Sabe-se que: Área = p * R2.
+Autor:Jules do Nascimento Pires
+Ex:010
+Versão:V1
+Data:03/07/2025
+'''
+
+# Bibliotecas
+import math
+import os
+
+# Limpa a tela
+os.system('cls')
+
+# Entrada de dados
+raio = float(input("Valor do raio:"))
+
+#Resultado
+area = math.pi * raio ** 2
+
+# Resultado
+print("Área do círculo {}:cm".format(area))
