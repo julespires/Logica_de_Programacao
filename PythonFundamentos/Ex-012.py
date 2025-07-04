0be36d973b5a23aa806f76d048e74dedfea6af84
Jules Pires <julespires@hotmail.com>
2025-07-04 19:12:53 -0300

    Create Ex-012.py

---
+++ PythonFundamentos/Ex-012.py
@@ -0,0 +1,23 @@
+'''
+Faça um programa que receba dois números maiores que zero, calcule e mostre um elevado ao outro.
+Autor:Jules do Nascimento Pires
+Ex-012
+Versão:V1
+Data:04/07/2025
+'''
+
+# Biblioteca
+import os
+
+os.system('cls')
+# Entrada de dados
+num1 = float(input("Digite o primeiro valor:"))
+num2 = float(input("Digite o segundo valor:"))
+
+# Cálculo
+r1 = num1 ** num2
+r2 = num2 ** num1
+
+# Resultado
+print("O número {} elevado a {} = {}".format(num1, num2, r1))
+print("O número {} elevado a {} = {}".format(num2, num1, r2))
