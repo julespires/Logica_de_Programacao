0ad68b224ad2f991fca289c0d660477fd317f7b7
Jules Pires <julespires@hotmail.com>
2025-07-07 16:15:39 -0300

    Ex-001.py
    Mudei Para subpasta

---
+++ PythonFundamentos/01-Estrutura sequencial/Ex-021.py
@@ -0,0 +1,14 @@
+import math
+
+# Entrada de dados
+angulo_graus = float(input("Digite o ângulo formado com o chão (em graus): "))
+altura = float(input("Digite a altura da parede (em metros): "))
+
+# Converter graus para radianos
+angulo_radianos = math.radians(angulo_graus)
+
+# Calcular o comprimento da escada
+escada = altura / math.sin(angulo_radianos)
+
+# Mostrar o resultado
+print(f"O comprimento da escada é: {escada:.2f} metros")
