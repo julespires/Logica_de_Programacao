a0468e9d87c3ae258029b3a14f97553797420ed6
Jules Pires <julespires@hotmail.com>
2025-07-07 16:45:00 -0300

    Estrutura condicionais
    Exercício 001

---
+++ PythonFundamentos/02-Estruturas condicionais/Ex-001.py
@@ -0,0 +1,21 @@
+'''
+Faça um programa que receba dois valores digitados pelo usuário e imprima o maior entre eles.
+Autor:Jules do Nascimento Pires
+Ex:001
+'''
+
+import os
+
+os.system('cls')
+
+# Entrada de dados
+num1 = int(input("Primeiro valor:"))
+num2 = int(input("Segundo valor:"))
+
+# Condições
+if num1 > num2:
+    print("O numero {} é o maior!".format(num1))
+if num2 > num1:
+    print("O número {} é o maior! ".format(num2))
+
+# Fim
